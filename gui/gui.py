cf6eebd454b0bf943d35eca1ca503152b0a72259
chickwensrule <80529810+ChickwensRule@users.noreply.github.com>
2023-01-26 00:30:20 +0800

    feat: info

--- gui/gui.py
+++ gui/gui.py
@@ -1,19 +1,53 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PyQt6.QtCore import Qt
 
 from camera import Camera
+from info import Info
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setStyleSheet("""
+            QMainWindow {
+                border-image: url("background.jpeg");
+            }
+        """)
+
 
         self.frame = QWidget()
         self.frame.layout = QVBoxLayout()
 
-        self.frame.layout.addWidget(Camera(1))
+        self.frame.layout.addWidget(Display())
+        self.frame.layout.addStretch()
         self.frame.setLayout(self.frame.layout)
 
         self.setCentralWidget(self.frame)
+
+        self.showMaximized()
+        
+class Display(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+
+        self.setStyleSheet("""
+            QWidget {
+                background: #3035d9;
+                border-radius: 20px;
+            }
+        """)
+
+
+        self.camera = Camera(1)
+        self.info = Info()
+
+        self.layout = QVBoxLayout()
+
+        self.layout.addWidget(self.camera)
+        self.layout.addWidget(self.info)
+
+        self.setLayout(self.layout)
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ gui/info.py
@@ -0,0 +1,34 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtCore import Qt
+
+class Info(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.tds = InfoLabel("TDS (water clarity)", "ppm")
+        self.ph = InfoLabel("pH", "")
+        self.weight = InfoLabel("Weight", "g")
+
+        self.layout = QVBoxLayout()
+        
+        self.layout.addWidget(self.tds)
+        self.layout.addWidget(self.ph)
+        self.layout.addWidget(self.weight)
+
+        self.setLayout(self.layout)
+
+class InfoLabel(QLabel):
+    def __init__(self, name, units):
+        super().__init__(f"{name}: 0 {units}")
+
+        self.setStyleSheet("""
+            font-weight: 700;
+
+            color: white;
+        """)
+
+        self.name = name
+        self.units = units
+
+    def update_value(self, v):
+        self.setText(f"{self.name}: {v} {self.units}")
